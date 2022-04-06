73c91753f27c3ddae82dcec681995ed202140c53
User <User@DESKTOP-802SV85>
2022-04-06 14:13:25 +0300

    task_3 done

--- Project4/Source0.cpp
+++ Project4/Source0.cpp
@@ -1,6 +1,50 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+const int SIZE = 20;
+
+void Set(int array[SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+        array[i] = rand() % 101 - 100;
+}
+
+void PRINT(int array[SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << array[i] << '\t';
+    }
+    cout << endl;
+}
+
+int prostie_chisla(int array[SIZE])
+{
+    int counter = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 2; j <= SIZE / 2; j++)
+        {
+            if (array[i] % j == 0 && array[i] != j)
+            {
+                array[i] = 0;
+                counter++;
+                break;
+            }
+        }
+        if (array[i] != 0)
+        {
+            cout << " " << array[i];
+        }
+    }
+    return 0;
+}
 
 int main()
 {
-	return 0;
+    srand(time(0));
+    int a[SIZE];
+    Set(a);
+    PRINT(a);
+    cout << " <-- Rezultat " << prostie_chisla(a) << endl;
 }
